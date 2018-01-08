6b2d2bc3110bf7cf72b15f63ec8944b890f90bb9
NickGatti <nick.gatti@gmail.com>
2018-01-08 11:04:14 -0700

    init

---
+++ server.js
@@ -0,0 +1,29 @@
+const express = require( 'express' );
+const app = express();
+const fs = require( 'fs' )
+const port = process.env.PORT || 8000;
+
+
+app.create( '/create/:name/:email/:state', function ( req, res ) {
+
+} )
+
+app.get( '/users', function ( req, res ) {
+
+} )
+
+app.get( '/user/:name', function ( req, res ) {
+
+} )
+
+app.update( '/user/:name', function ( req, res ) {
+
+} )
+
+app.delete( '/user/:name', function ( req, res ) {
+
+} )
+
+app.use( function ( req, res ) {
+    res.sendStatus( 404 )
+} )
