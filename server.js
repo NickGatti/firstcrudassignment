9a8cc6097a9ab77084a1051287e359c54362fda5
NickGatti <nick.gatti@gmail.com>
2018-01-08 13:40:14 -0700

    stretch

--- server.js
+++ server.js
@@ -9,6 +9,7 @@ app.post( '/create/:name/:email/:state', function ( req, res ) {
         let output = fs.readFileSync( './storage.json', 'utf-8' )
         output = JSON.parse( output )
         output.push( {
+            id: output.length,
             name: req.params.name,
             email: req.params.email,
             state: req.params.state
@@ -23,26 +24,28 @@ app.get( '/users', function ( req, res ) {
     res.send( fs.readFileSync( './storage.json', 'utf-8' ) )
 } )
 
-app.get( '/user/:name', function ( req, res ) {
-    if ( req.params.name ) {
+app.get( '/user/:id', function ( req, res ) {
+    if ( req.params.id ) {
         let output = fs.readFileSync( './storage.json', 'utf-8' )
         output = JSON.parse( output )
         for ( let i = 0; i < output.length; i++ ) {
-            if ( output[ i ].name === req.params.name ) {
+            if ( output[ i ].id === req.params.id ) {
                 res.setHeader( 'Content-Type', 'text/json' );
                 res.send( output[ i ] )
                 return;
             }
         }
+    } else {
+        res.sendStatus( 404 )
     }
 } )
 
-app.post( '/user/edit/:name/:email/:state', function ( req, res ) {
-    if ( req.params.name && req.params.email && req.params.state ) {
+app.post( '/user/edit/:id/:name/:email/:state', function ( req, res ) {
+    if ( req.params.id && req.params.name && req.params.email && req.params.state ) {
         let output = fs.readFileSync( './storage.json', 'utf-8' )
         output = JSON.parse( output )
         for ( let i = 0; i < output.length; i++ ) {
-            if ( output[ i ].name === req.params.name ) {
+            if ( output[ i ].id == req.params.id ) {
                 output[ i ].name = req.params.name
                 output[ i ].email = req.params.email
                 output[ i ].state = req.params.state
@@ -54,11 +57,11 @@ app.post( '/user/edit/:name/:email/:state', function ( req, res ) {
     }
 } )
 
-app.post( '/user/delete/:name', function ( req, res ) {
+app.post( '/user/delete/:id', function ( req, res ) {
     let output = fs.readFileSync( './storage.json', 'utf-8' )
     output = JSON.parse( output )
     for ( let i = 0; i < output.length; i++ ) {
-        if ( output[ i ].name === req.params.name ) {
+        if ( output[ i ].id === req.params.id ) {
             output.splice( i, 1 )
             fs.writeFileSync( 'storage.json', JSON.stringify( output ) )
             res.sendStatus( 200 )
